6bb9a648f9a6289e836b1a876beef71cca85b039
chonie1 <connie3@ualberta.ca>
2021-01-31 01:29:53 +0000

    completed styling for desktops

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  
+  //Change copyright date every year
+  let current = new Date()
+  year = current.getFullYear()
+  console.log(year)
+  $("#year").text(`${year}`)
+
+
+  //Toggle dark mode
+    function darkMode() {
+      $(".card").toggleClass("dark-mode")
+      if ($("#role").text() == "full stack web developer") {  
+        $("#role").text("chemical biomedical engineer")
+        $("#background").attr("src","/images/background-dark2.jpg")
+      } else {
+        $("#role").text("full stack web developer")
+        $("#background").attr("src","/images/background3.jpg")
+      }
+      
+    }
+
+    function changeBackground () {
+      if ($("#role").text() == "full stack web developer") {  
+        $("body").css("background","linear-gradient(#B1BDDD, #AA878A)")
+      } else {
+        $("body").css("background","linear-gradient(#11111B, #112242)")
+      }
+    }
+
+    $("#dark-mode").click(function() {
+      $("body").fadeOut(1000)
+      setTimeout(darkMode,1000)
+      $("body").fadeIn(1000)
+      setTimeout(changeBackground,2000)
+      
+    });
+
+})
+
+
